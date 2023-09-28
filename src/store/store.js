fa9d5a5a77f5de72be4ec4bfabfe334fd6a57595
박지윤 <tkrhkfld123@naver.com>
2023-09-28 14:01:59 +0900

    ✅ [MOD] redux-toolkit 초기 세팅

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const a = createSlice({
+  name: 'a',
+  initialState: 'test',
+  reducers: {
+  	test(state, action) {
+    }
+  }
+})
+
+export default configureStore({
+  reducer: {
+  	a: a.reducer
+  },
+});
