7d346df03b48910e39e096be8c2e2fc71ec15bb2
박지윤 <tkrhkfld123@naver.com>
2023-10-13 17:29:46 +0900

    ➕ [ADD] 마이페이지에 정보 나타내기

---
+++ src/routes/MyPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+const MyPage = () => {
+    const profileImage = useSelector((state) => state.user.profileImage);
+    const nickname = useSelector((state) => state.user.nickname);
+  return (
+  <div className='grid place-items-center'>
+    <h1>마이페이지</h1>
+    <img className='w-[500px] h-[500px] rounded-[50%]' src={profileImage} alt="프로필 이미지" />
+    <p>{nickname}님 어서오세요 !</p>
+  </div>
+  )
+}
+
+export default MyPage
