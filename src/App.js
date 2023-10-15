f2b66e9cc0858f360acf543809eda4fd55bab8cb
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-15 15:25:03 +0900

    :recycle: [REFACTOR] RouteProvider로 router 정보 전달

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import Header from 'components/Header/Header';
 import 'style/globals.css';
-import Main from 'pages/MainPage';
-import { RecommandVariableName } from 'pages/RecommandVariableNamePage';
-import ChangeCodeLanguage from 'pages/ChangeCodeLanguagePage';
-import { RefactorCode } from 'pages/RefactorCodePage';
-import { AddComment } from 'pages/AddCommentPage';
-import { RecommandContent } from 'pages/RecommandContentPage';
-import { Route, Routes } from 'react-router-dom';
-import KakaoRedirect from 'components/Login/KakaoRedirect';
-import MyPage from 'pages/MyPage';
+import { RouterProvider } from 'react-router-dom';
+import { routers } from 'router';
 
 export default function App() {
   return (
-    <div className='flex flex-col h-screen'>
-      <Header />
-      {/* <Main /> */}
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='recommand-variable-name' element={<RecommandVariableName />} />
-        <Route path='change-language' element={<ChangeCodeLanguage />} />
-        <Route path='refactor' element={<RefactorCode />} />
-        <Route path='add-comment' element={<AddComment />} />
-        <Route path='recommand-content' element={<RecommandContent />} />
-        <Route path='kakao/user' element={<KakaoRedirect />} />
-        <Route path='mypage' element={<MyPage />} />
-      </Routes>
-    </div>
+    <>
+      <RouterProvider router={routers} />
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from 'App';
 import { Provider } from 'react-redux';
 import store from 'store/store';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <CookiesProvider>
     <Provider store={store}>
-      <BrowserRouter basename='/'>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </CookiesProvider>
 );
