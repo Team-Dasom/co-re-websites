a649f01fb4b284f6b92a3227298725efa163fdd6
박지윤 <tkrhkfld123@naver.com>
2023-10-16 19:26:50 +0900

    ➕ [ADD] 언어 선택 드롭박스

--- src/components/VariableName/InputDropDown.js
+++ src/components/VariableName/InputDropDown.js
@@ -1,11 +1,15 @@
 // 'C', 'C#', 'C++', 'Dart', 'Go','Java','Javascript','typescript','kotlin'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const option = ['C', 'C#', 'C++', 'Dart', 'Go','Java','Javascript','typescript','kotlin']
 
 const InputDropDown = () => {   
     const [selectedItem, setSelectedItem] = useState('');
+
+    useEffect(()=>{
+        
+    },[selectedItem])
 
   return (
     <div className='top-px w-[175px] h-[200px] border overflow-y-scroll shadow-2xl mt-[5px] rounded-[10px] cursor-pointer bg-white'>
@@ -14,10 +18,10 @@ const InputDropDown = () => {
                     return(
                         <li
                         className='hover:bg-[#f2f2f2]'
-                        key={index} onClick={() => {
-                            console.log(item) // 여기서 콘솔로 찍어보면 잘 나옴
-                            setSelectedItem(item)
-                            console.log(selectedItem) // 여기서 콘솔로 찍으면 빈 값이 나와여 ;;;; 개어이없음
+                        key={index} 
+                        onClick={(e) => {
+                            setSelectedItem({item})
+                            console.log(selectedItem)
                         }}>{item}</li>
                     )
                 })
