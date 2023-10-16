400d031a92cd590187339c1bee553cb86fe58b42
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-10-16 21:25:13 +0900

    ✨ [FEAT/#20] 변수 추천 기능 구현 (#23)
    * ➕ [ADD] 변수명 추천 페이지에 내 프로필 입력받기
    
    * ⚰️ [DEL] 쓸모 없는 파일 삭제
    
    * ➕ [ADD] 쿠키 관련 토드 작성
    
    * ✅ [MOD] 서버 accessToken 쿠키에 저장
    
    * ✨ [FEAT] 변수추천 기능 구현
    
    * 💄 [UI] 언어변경 dropdwon ui 구현
    
    * ➕ [ADD] 언어 선택 드롭박스
    
    * ➕ [ADD] dropdwon 코드 추가
    
    * ⚰️ [DEL] 변수추천 페이지 삭제

---
+++ src/components/VariableName/VariableName.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import logo from '../../images/logo-icon.png'
+import { useSelector } from 'react-redux';
+
+
+const VariableName = ({inputString, variableData}) => {
+    const profileImage = useSelector((state) => state.user.profileImage);
+
+return (
+    <div>
+        <div className='ml-[-200px]'>
+        {/* 내가 요청한 변수 */}
+            <div className='flex h-auto items-center p-[25px]'>
+                <img className='w-[64px] h-[64px] ml-[360px] rounded-[10px]' src={profileImage} alt="Img" />
+                <div className='ml-[25px]'>{inputString}</div>
+            </div>
+
+        {/* 서버에서 가지고 온 내용 */}
+            <div className='bg-[#EDEDED] h-auto pl-[200px] p-[25px] '>
+                <div className='flex items-center'>
+                    <img className='w-[68px] h-[68px] ml-[180px]' src={logo} alt="logo" />
+                    <div className='ml-[25px] '> 
+                    {
+                    variableData.split('\n').map((data, index)=>{ // variableData에 응답 결과 저장 및 \n 을 <br />태그로 다시 렌더링
+                        return(
+                            <div>
+                                {data} <br />
+                            </div>
+                            )
+                        })
+                    }
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+)
+}
+
+export default VariableName
