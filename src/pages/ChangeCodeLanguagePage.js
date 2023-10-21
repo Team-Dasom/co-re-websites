9aae871d5eb7f9f5e70c091dbd78b1376c2c5373
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-22 02:11:46 +0900

    :lipstick: [UI] textarea 일정 부분까지 늘어나게 구현

--- src/pages/ChangeCodeLanguagePage.js
+++ src/pages/ChangeCodeLanguagePage.js
@@ -6,6 +6,19 @@ export default function ChangeCodeLanguage() {
   const languageList = ['C', 'C#', 'C++', 'Dart', 'Go', 'Java', 'Javascript', 'typescript', 'kotlin'];
   const { register, handleSubmit } = useForm();
 
+  const textAreaAutosize = (e) => {
+    const element = e.target
+    element.style.height = 'auto';
+    const maxHeight = 280;
+    
+    if(element.scrollHeight <= maxHeight) {
+      console.log(element.scrollHeight);
+      element.style.height = `${element.scrollHeight}px`;
+    } else {
+      element.style.height = `${maxHeight}px`;
+    }
+  }
+
   const onSubmit = (data) => {
     console.log(data);
   };
@@ -27,7 +40,9 @@ export default function ChangeCodeLanguage() {
               rows={1}
               className='resize-none focus:outline-none mt-2 border-[#3B82F6] border-solid border-[1px] p-2 w-[calc(100%-7.75rem)] h-12 rounded-md'
               placeholder='언어를 변경할 코드를 입력하세요.'
-              {...register('//들어갈 이름//')}
+              {...register('//들어갈 이름//', {
+                onChange: textAreaAutosize,
+              })}
             />
             <input
               className='bg-[#3B82F6] text-white w-28 h-12 rounded-lg cursor-pointer ml-3'
