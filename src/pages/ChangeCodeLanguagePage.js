37e244294fa1ca7e055ac9a81b9c85d36e1daaa1
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-22 19:17:56 +0900

    :sparkles: [FEAT] Code 언어 변경 기능 구현
    변경을 원하는 코드와 변경할 언어를 서버측에 보낸 후 응답 받은 변경된 언어 화면상 출력

--- src/pages/ChangeCodeLanguagePage.js
+++ src/pages/ChangeCodeLanguagePage.js
@@ -1,35 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Dropdown from 'components/Dropdown';
 import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { addAnswer, addQuestion } from 'store/changeLanguage/changeLanguageSlice';
+import Answer from 'components/conversation/Answer';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import axios from 'axios';
 
 export default function ChangeCodeLanguage() {
-  const languageList = ['C', 'C#', 'C++', 'Dart', 'Go', 'Java', 'Javascript', 'typescript', 'kotlin'];
-  const { register, handleSubmit } = useForm();
+  const languageList = [
+    'C',
+    'C#',
+    'C++',
+    'Dart',
+    'Go',
+    'Java',
+    'Javascript',
+    'typescript',
+    'kotlin',
+  ];
+  const { register, handleSubmit, reset, getValues } = useForm();
+  const conversation = useSelector(
+    (state) => state.changeLanguage.conversation,
+  );
+  const dispatch = useDispatch();
 
   const textAreaAutosize = (e) => {
-    const element = e.target
+    const element = e.target;
     element.style.height = 'auto';
     const maxHeight = 280;
-    
-    if(element.scrollHeight <= maxHeight) {
-      console.log(element.scrollHeight);
+
+    if (element.scrollHeight <= maxHeight) {
       element.style.height = `${element.scrollHeight}px`;
     } else {
       element.style.height = `${maxHeight}px`;
     }
-  }
-
-  const onSubmit = (data) => {
-    console.log(data);
   };
-  
-  return (
-    <div className='w-full h-[calc(100vh-4rem)] align-center'>
-      <div className='flex justify-center'>user</div>
-      <div className='flex justify-center'>chatGPT</div>
 
-      <div className='absolute w-2/3 -translate-x-1/2 bottom-10 left-1/2'>
-        <div className='flex justify-center'>
+  const handleKeyPress = async (e) => {
+    if (e.key === 'Enter' && e.nativeEvent.isComposing === false) {
+      if (!e.shiftKey) {
+        e.preventDefault();
+        dispatch(addAnswer(getValues().content));
+        reset();
+        const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/changeLanguage`, {
+          function: "CHANGE_LANGUAGE",
+          ...getValues()
+        });
+
+        console.log(res);
+
+        e.target.style.height = 'auto';
+      }
+    }
+  };
+
+  const onSubmit = async (data) => {
+    dispatch(addAnswer(data.content));
+    reset();
+    const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/changeLanguage`, {
+      function: "CHANGE_LANGUAGE",
+      ...data
+    });
+
+    dispatch(addQuestion(res.data.data.content));
+  };
+
+  useEffect(() => {}, [conversation])
+
+  return (
+    <ScrollToBottom className='w-full h-[calc(100vh-4rem)] align-center overflow-scroll'>
+        {conversation.map((item) => {
+          return <Answer key={item.id} data={item} isAnswer={item.isAnswer} />;
+        })}
+      <div className='fixed bottom-0 w-2/3 -translate-x-1/2 left-1/2'>
+        <div className='flex flex-col justify-center'>
           <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
             <Dropdown
               list={languageList}
@@ -38,9 +83,10 @@ export default function ChangeCodeLanguage() {
             />
             <textarea
               rows={1}
-              className='resize-none focus:outline-none mt-2 border-[#3B82F6] border-solid border-[1px] p-2 w-[calc(100%-7.75rem)] h-12 rounded-md'
+              className='align-bottom resize-none focus:outline-none mt-2 border-[#3B82F6] border-solid border-[1px] p-2 w-[calc(100%-7.75rem)] h-12 rounded-md'
               placeholder='언어를 변경할 코드를 입력하세요.'
-              {...register('//들어갈 이름//', {
+              onKeyDown={handleKeyPress}
+              {...register('content', {
                 onChange: textAreaAutosize,
               })}
             />
@@ -50,8 +96,10 @@ export default function ChangeCodeLanguage() {
               value='submit'
             />
           </form>
+          <div className='w-full h-12 bg-white '></div>
         </div>
       </div>
-    </div>
+      <div className='flex-shrink-0 h-36 md:h-48'></div>
+    </ScrollToBottom>
   );
 }
