b6d6d7e15c927198c278c1a83e77166079f86c87
Largopie <106071687+Largopie@users.noreply.github.com>
2023-11-01 09:41:57 +0900

    :recycle: [REFACTOR] Code 언어 변경 -> 알고리즘 문제 해설 문제로 변경
    - 연결 API 변경\n- 알고리즘과 관련된 form으로 변경

--- src/pages/SolveAlgorithmPage.js
+++ src/pages/SolveAlgorithmPage.js
@@ -8,86 +8,77 @@ import ScrollToBottom from 'react-scroll-to-bottom';
 import axios from 'axios';
 
 export default function SolveAlgorithm() {
-  const languageList = ['C', 'C#', 'C++', 'Dart', 'Go', 'Java', 'Javascript', 'typescript', 'kotlin'];
+  const languageList = ['Python3', 'C', 'Java', 'Ruby', 'Kotlin', 'Swift', 'C#', 'Node.js', 'Go', 'D', 'Rust', 'C++'];
   const { register, handleSubmit, reset, getValues } = useForm();
-  const conversation = useSelector(
-    (state) => state.changeLanguage.conversation,
-  );
+  const conversation = useSelector((state) => state.changeLanguage.conversation);
   const dispatch = useDispatch();
-
-  const textAreaAutosize = (e) => {
-    const element = e.target;
-    element.style.height = 'auto';
-    const maxHeight = 280;
-
-    if (element.scrollHeight <= maxHeight) {
-      element.style.height = `${element.scrollHeight}px`;
-    } else {
-      element.style.height = `${maxHeight}px`;
-    }
-  };
 
   const handleKeyPress = async (e) => {
     if (e.key === 'Enter' && e.nativeEvent.isComposing === false) {
       if (!e.shiftKey) {
         e.preventDefault();
         const values = getValues();
-        dispatch(addQuestion(values.content));
+        dispatch(addQuestion(values.problem));
         reset();
-        const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/changeLanguage`, {
-          function: "CHANGE_LANGUAGE",
-          ...values
+        const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/solveAlgorithm`, {
+          function: 'SOLVE_ALGORITHM',
+          ...values,
         });
-
-        dispatch(addAnswer(res.data.data.content))
-
-        e.target.style.height = 'auto';
+        dispatch(addAnswer(res.data.data.content));
       }
     }
   };
 
   const onSubmit = async (data) => {
-    dispatch(addQuestion(data.content));
+    dispatch(addQuestion(data.problem));
     reset();
-    const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/changeLanguage`, {
-      function: "CHANGE_LANGUAGE",
-      ...data
+    const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/solveAlgorithm`, {
+      function: 'SOLVE_ALGORITHM',
+      ...data,
     });
-
+    
     dispatch(addAnswer(res.data.data.content));
   };
 
-  useEffect(() => {}, [conversation])
-
+  useEffect(() => {}, [conversation]);
+  
   return (
-    <ScrollToBottom className='w-full h-[calc(100vh-4rem)] align-center overflow-scroll'>
-        {conversation.map((item) => {
-          return <Answer key={item.id} data={item} isAnswer={item.isAnswer} />;
-        })}
-      <div className='fixed bottom-0 w-2/3 -translate-x-1/2 left-1/2'>
+    <ScrollToBottom className='w-full min-h-[calc(100vh-4rem)] align-center overflow-scroll'>
+      {conversation.map((item) => {
+        return <Answer key={item.id} data={item} isAnswer={item.isAnswer} />;
+      })}
+      <div className='bottom-[-1.25rem] fixed w-[100vw] h-[23vh] bg-white blur-xl'></div>
+      <div className='fixed bottom-0 w-2/3 -translate-x-1/2 bg-white left-1/2'>
         <div className='flex flex-col justify-center'>
           <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
-            <Dropdown
-              list={languageList}
-              title={'language'}
-              register={register}
-            />
-            <textarea
-              rows={1}
-              className='align-bottom resize-none focus:outline-none mt-2 border-[#3B82F6] border-solid border-[1px] p-2 w-[calc(100%-7.75rem)] h-12 rounded-md'
-              placeholder='언어를 변경할 코드를 입력하세요.'
-              onKeyDown={handleKeyPress}
-              {...register('content', {
-                onChange: textAreaAutosize,
-              })}
-            />
+            <div className='flex gap-3'>
+              <Dropdown list={languageList} title={'language'} register={register} />
+              <div className='w-48'>
+                <label htmlFor='platform' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  플랫폼을 선택하세요.
+                </label>
+                <select
+                  id='platform'
+                  {...register('platform')}
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#3B82F6] focus:border-[#3B82F6] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#3B82F6] dark:focus:border-[#3B82F6]'
+                >
+                  <option value=''>선택하세요</option>
+                  <option value='백준'>백준</option>
+                  <option disabled value='프로그래머스'>
+                    프로그래머스
+                  </option>
+                </select>
+              </div>
+            </div>
             <input
-              className='bg-[#3B82F6] text-white w-28 h-12 rounded-lg cursor-pointer ml-3'
-              type='submit'
-              value='submit'
+              className='align-bottom resize-none focus:outline-none mt-2 border-[#3B82F6] border-solid border-[1px] p-2 w-[calc(100%-7.75rem)] h-12 rounded-md'
+              placeholder='해결할 문제 번호를 입력하세요.'
+              onKeyDown={handleKeyPress}
+              {...register('problem')}
             />
+            <input className='bg-[#3B82F6] hover:bg-[#9bbffa] text-white w-28 h-12 rounded-lg cursor-pointer ml-3' type='submit' value='submit' />
           </form>
-          <div className='w-full h-12 bg-white '></div>
+          <div className='w-full h-3 bg-white '></div>
         </div>
       </div>
       <div className='flex-shrink-0 h-36 md:h-48'></div>
