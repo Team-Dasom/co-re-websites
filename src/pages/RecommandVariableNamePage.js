e0b327d3d234ae43cb0d05ad7f3a75cb8a2c41a4
Largopie <106071687+Largopie@users.noreply.github.com>
2023-11-02 13:52:10 +0900

    ♻️ [REFACTOR] TextAreaForm으로 코드 수정

--- src/pages/RecommandVariableNamePage.js
+++ src/pages/RecommandVariableNamePage.js
@@ -1,88 +1,15 @@
 import React, { useEffect } from 'react';
-import axios from 'axios';
-import Dropdown from 'components/Dropdown';
-import { useForm } from 'react-hook-form';
 import ScrollToBottom from 'react-scroll-to-bottom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { addAnswer, addQuestion } from 'store/variebleName/variebleNameSlice';
 import Answer from 'components/conversation/Conversation';
+import TextAreaForm from 'components/Form/TextAreaForm';
 
 export default function RecommandVariableName() {
-  const { register, handleSubmit, reset, getValues } = useForm();
   const conversation = useSelector(
     (state) => state.variebleName.conversation,
   );
-  const dispatch = useDispatch();
   const languageList = ['C', 'C#', 'C++', 'Dart', 'Go', 'Java', 'Javascript', 'typescript', 'kotlin'];
-
-
-  const textAreaAutosize = (e) => {
-    const element = e.target;
-    element.style.height = 'auto';
-    const maxHeight = 280;
-
-    if (element.scrollHeight <= maxHeight) {
-      element.style.height = `${element.scrollHeight}px`;
-    } else {
-      element.style.height = `${maxHeight}px`;
-    }
-  };
-
-  const handleKeyPress = async (e) => {
-    if (e.key === 'Enter' && e.nativeEvent.isComposing === false) {
-      if (!e.shiftKey) {
-        e.preventDefault();
-        const values = getValues();
-        dispatch(addQuestion(values.content));
-        reset();
-
-        try {
-          const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/recommendVariableName`, {
-            function: 'RECOMMEND_VARIABLE_NAME',
-            ...values,
-          });
-
-          dispatch(addAnswer(res.data.data.content));
-
-          e.target.style.height = '46px';
-        } catch (error) {
-          if (error.response && error.response.status === 400) {
-            console.log(error.toJSON());
-            alert('언어를 선택해 주세요 !');
-            setTimeout(() => {
-              e.target.style.height = '46px';
-            }, 0);          
-          } else {
-            console.error('Error:', error);
-          }
-        }
-      }
-    }
-  };
-
-  const onSubmit = async (data) => {
-    dispatch(addQuestion(data.content));
-    reset();
-
-    try {
-      const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/recommendVariableName`, {
-        function: 'RECOMMEND_VARIABLE_NAME',
-        ...data,
-      });
-
-      dispatch(addAnswer(res.data.data.content));
-    } catch (error) {
-      if (error.response && error.response.status === 400) {
-        console.log(error.toJSON());
-        alert('언어를 선택해 주세요 !');
-        setTimeout(() => {
-          document.getElementById('commentTextArea').style.height = '46px';     
-        }, 0);       
-      } else {
-        console.error('Error:', error);
-      }
-    }
-  };
 
   useEffect(() => {}, [conversation])
 
@@ -91,34 +18,13 @@ export default function RecommandVariableName() {
         {conversation.map((item) => {
           return <Answer key={item.id} data={item} isAnswer={item.isAnswer} />;
         })}
-      <div className='fixed bottom-0 w-2/3 -translate-x-1/2 left-1/2'>
-        <div className='flex flex-col justify-center'>
-          <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
-            <Dropdown
-              list={languageList}
-              title={'language'}
-              register={register}
-            />
-            <textarea
-            id='commentTextArea'
-              rows={1}
-              className='align-bottom resize-none focus:outline-none mt-2 border-[#3B82F6] border-solid border-[1px] p-2 w-[calc(100%-7.75rem)] h-12 rounded-md'
-              placeholder='추천을 원하는 "변수"명만 입력해 보세요.'
-              onKeyDown={handleKeyPress}
-              {...register('content', {
-                onChange: textAreaAutosize,
-              })}
-            />
-            <input
-              className='bg-[#3B82F6] text-white w-28 h-12 rounded-lg cursor-pointer ml-3'
-              type='submit'
-              value='submit'
-            />
-          </form>
-          <div className='w-full h-12 bg-white '></div>
-        </div>
-      </div>
-      <div className='flex-shrink-0 h-36 md:h-48'></div>
+        <TextAreaForm
+          camelCaseAPI='recommendVariableName'
+          placeholder='추천을 원하는 "변수"명만 입력해 보세요.'
+          addQuestion={addQuestion}
+          addAnswer={addAnswer}
+          dropdownList={languageList}/>
+        <div className='flex-shrink-0 h-36 md:h-48'></div>
     </ScrollToBottom>
   )
 }
--- src/pages/RefactorCodePage.js
+++ src/pages/RefactorCodePage.js
@@ -1,85 +1,13 @@
 import React, { useEffect } from 'react';
-import Dropdown from 'components/Dropdown';
-import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
 import Conversation from 'components/conversation/Conversation';
 import ScrollToBottom from 'react-scroll-to-bottom';
-import axios from 'axios';
 import { addAnswer, addQuestion } from 'store/refactorCode/refactorCodeSlice';
+import TextAreaForm from 'components/Form/TextAreaForm';
+import { useSelector } from 'react-redux';
 
 export default function RefactorCode() {
   const languageList = ['Python3', 'C', 'Java', 'Ruby', 'Kotlin', 'Swift', 'C#', 'JavaScript', 'TypeScript', 'Go', 'D', 'Rust', 'C++'];
-  const { register, handleSubmit, reset, getValues } = useForm();
   const conversation = useSelector((state) => state.refactorCode.conversation);
-  const dispatch = useDispatch();
-
-  const textAreaAutosize = (e) => {
-    const element = e.target;
-    element.style.height = 'auto';
-    const maxHeight = 280;
-
-    if (element.scrollHeight <= maxHeight) {
-      element.style.height = `${element.scrollHeight}px`;
-    } else {
-      element.style.height = `${maxHeight}px`;
-    }
-  };
-
-  const handleKeyPress = async (e) => {
-    if (e.key === 'Enter' && e.nativeEvent.isComposing === false) {
-      if (!e.shiftKey) {
-        e.preventDefault();
-        const values = getValues();
-        dispatch(addQuestion(values.content));
-        reset();
-
-        try {
-          const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/refactorCode`, {
-            function: 'REFACTOR_CODE',
-            ...values,
-          });
-
-          dispatch(addAnswer(res.data.data.content));
-
-          e.target.style.height = '46px';
-        } catch (error) {
-          if (error.response && error.response.status === 400) {
-            console.log(error.toJSON());
-            alert('언어를 선택해 주세요 !');
-            setTimeout(() => {
-              e.target.style.height = '46px';
-            }, 0);  
-          } else {
-            console.error('Error:', error);
-          }
-        }
-      }
-    }
-  };
-
-  const onSubmit = async (data) => {
-    dispatch(addQuestion(data.content));
-    reset();
-
-    try {
-      const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/refactorCode`, {
-        function: 'REFACTOR_CODE',
-        ...data,
-      });
-
-      dispatch(addAnswer(res.data.data.content));
-    } catch (error) {
-      if (error.response && error.response.status === 400) {
-        console.log(error.toJSON());
-        alert('언어를 선택해 주세요 !');
-        setTimeout(() => {
-          document.getElementById('commentTextArea').style.height = '46px';     
-        }, 0); 
-      } else {
-        console.error('Error:', error);
-      }
-    }
-  };
 
   useEffect(() => {}, [conversation]);
 
@@ -88,26 +16,12 @@ export default function RefactorCode() {
       {conversation.map((item) => {
         return <Conversation key={item.id} data={item} isAnswer={item.isAnswer} />;
       })}
-      <div className='bottom-[-1.25rem] fixed w-[100vw] h-[23vh] bg-white blur-xl'></div>
-      <div className='fixed bottom-0 w-2/3 -translate-x-1/2 bg-white left-1/2'>
-        <div className='flex flex-col justify-center'>
-          <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
-            <Dropdown list={languageList} title={'language'} register={register} />
-            <textarea
-              id='commentTextArea'
-              rows={1}
-              className='align-bottom resize-none focus:outline-none mt-2 border-[#3B82F6] border-solid border-[1px] p-2 w-[calc(100%-7.75rem)] h-12 rounded-md'
-              placeholder='언어를 변경할 코드를 입력하세요.'
-              onKeyDown={handleKeyPress}
-              {...register('content', {
-                onChange: textAreaAutosize,
-              })}
-            />
-            <input className='bg-[#3B82F6] hover:bg-[#9bbffa] text-white w-28 h-12 rounded-lg cursor-pointer ml-3' type='submit' value='submit' />
-          </form>
-          <div className='w-full h-3 bg-white '></div>
-        </div>
-      </div>
+      <TextAreaForm
+      camelCaseAPI='refactorCode'
+      placeholder='리팩토링할 코드를 입력해주세요.'
+      addQuestion={addQuestion}
+      addAnswer={addAnswer}
+      dropdownList={languageList}/>
       <div className='flex-shrink-0 h-36 md:h-48'></div>
     </ScrollToBottom>
   );
