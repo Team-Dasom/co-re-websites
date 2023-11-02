2c9238d9781945f983e2d76ef4a6efd4b7e20674
Largopie <106071687+Largopie@users.noreply.github.com>
2023-11-02 13:29:11 +0900

    ✨ [FEAT] textarea 공용 form 구현

--- src/components/Form/TextAreaForm.js
+++ src/components/Form/TextAreaForm.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import Dropdown from 'components/Dropdown';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+import ReactTextareaAutosize from 'react-textarea-autosize';
 
 const isUpperCase = (char) => {
   return char === char.toUpperCase();
@@ -9,38 +11,34 @@ const isUpperCase = (char) => {
 
 const camelCaseToSnakeCase = (str) => {
   const strArray = str.split('');
-  let newStr = strArray
-  .map((charactor) => (isUpperCase(charactor) ? `_${charactor.toUpperCase()}` : charactor.toUpperCase()))
-  .join('');
+  let newStr = strArray.map((charactor) => (isUpperCase(charactor) ? `_${charactor.toUpperCase()}` : charactor.toUpperCase())).join('');
 
   return newStr;
 };
 
 export default function TextAreaForm({ camelCaseAPI, placeholder, addQuestion, addAnswer, dropdownList }) {
-  const { register, handleSubmit, reset, getValues } = useForm();
+  const { register, handleSubmit, reset, getValues, setFocus } = useForm();
   const dispatch = useDispatch();
 
   const onSubmit = async (data) => {
     const inputData = data;
     dispatch(addQuestion(inputData.content));
     reset();
-    const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/${camelCaseAPI}`, {
-      function: camelCaseToSnakeCase(camelCaseAPI),
-      ...inputData,
-    });
+    
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/${camelCaseAPI}`, {
+        function: camelCaseToSnakeCase(camelCaseAPI),
+        ...inputData,
+      });
 
-    dispatch(addAnswer(res.data.data.content));
-  };
-
-  const textAreaAutosize = (e) => {
-    const element = e.target;
-    element.style.height = 'auto';
-    const maxHeight = 280;
-
-    if (element.scrollHeight <= maxHeight) {
-      element.style.height = `${element.scrollHeight}px`;
-    } else {
-      element.style.height = `${maxHeight}px`;
+      dispatch(addAnswer(res.data.data.content));
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        console.log(error.toJSON());
+        alert('언어를 선택해 주세요 !');
+      } else {
+        console.error('Error:', error);
+      }
     }
   };
 
@@ -49,35 +47,53 @@ export default function TextAreaForm({ camelCaseAPI, placeholder, addQuestion, a
       if (!e.shiftKey) {
         e.preventDefault();
         const values = getValues();
-        dispatch(addQuestion(values.content));
         reset();
-        const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/${camelCaseAPI}`, {
-          function: camelCaseToSnakeCase(camelCaseAPI),
-          ...values,
-        });
+        dispatch(addQuestion(values.content));
+        e.target.rows = 1;
+        
+        try {
+          const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/gpt/${camelCaseAPI}`, {
+            function: camelCaseToSnakeCase(camelCaseAPI),
+            ...values,
+          });
 
-        dispatch(addAnswer(res.data.data.content));
-
-        e.target.style.height = 'auto';
+          dispatch(addAnswer(res.data.data.content));
+        } catch (error) {
+          if (error.response && error.response.status === 400) {
+            console.log(error.toJSON());
+            alert('언어를 선택해 주세요 !');
+          } else {
+            console.error('Error:', error);
+          }
+        }
       }
     }
   };
 
+  useEffect(() => {
+    setFocus('content');
+  }, [setFocus]);
+
   return (
     <>
-      <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
-        <Dropdown list={dropdownList} title={'language'} register={register} />
-        <textarea
-          rows={1}
-          className='align-bottom resize-none focus:outline-none mt-2 border-[#3B82F6] border-solid border-[1px] p-2 w-[calc(100%-7.75rem)] h-12 rounded-md'
-          placeholder={placeholder}
-          onKeyDown={handleKeyPress}
-          {...register('content', {
-            onChange: textAreaAutosize,
-          })}
-        />
-        <input className='bg-[#3B82F6] hover:bg-[#9bbffa] text-white w-28 h-12 rounded-lg cursor-pointer ml-3' type='submit' value='submit' />
-      </form>
+      <div className='bottom-[-1.25rem] fixed w-[100vw] h-[23vh] bg-white blur-xl'></div>
+      <div className='fixed bottom-0 w-2/3 -translate-x-1/2 left-1/2'>
+        <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
+          <Dropdown list={dropdownList} title={'language'} register={register} />
+          <ReactTextareaAutosize
+            minRows={1}
+            maxRows={6}
+            className='align-bottom resize-none focus:outline-none mt-2 border-[#3B82F6] border-solid border-[1px] h-[46px] p-2 w-[calc(100%-7.75rem)] rounded-md'
+            placeholder={placeholder}
+            onKeyDown={handleKeyPress}
+            {...register('content', {
+              required: true,
+            })}
+          />
+          <input className='bg-[#3B82F6] hover:bg-[#9bbffa] text-white w-28 h-12 rounded-lg cursor-pointer ml-3' type='submit' value='submit' />
+        </form>
+        <div className='w-full h-3 bg-white '></div>
+      </div>
     </>
   );
 }
