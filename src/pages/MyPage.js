5f48b1df7b49ec83b5344f8f18ac1ae8c6b6a8cb
박지윤 <tkrhkfld123@naver.com>
2023-11-05 16:47:50 +0900

    ✨ [FEAT] 로그아웃 기능 구현

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,16 +1,25 @@
-import { getCookie } from 'components/Cookie/Cookies';
+import { getCookie, removeCookie } from 'components/Cookie/Cookies';
+import { useRouter } from 'hooks/useRouter';
 import React from 'react'
 import { useSelector } from 'react-redux';
 
 export default function MyPage() {
     const profileImage = useSelector((state) => state.user.profileImage);
     const nickname = useSelector((state) => state.user.nickname);
-    const name = getCookie('nickname');
+    const { routeTo, currentPath } = useRouter();
+
+    const isLogout = () => {
+      removeCookie('nickname')
+      removeCookie('profileImage')
+      window.location.href = '/';
+    }
+
   return (
   <div className='grid place-items-center'>
     <h1>마이페이지</h1>
     <img className='w-[500px] h-[500px] rounded-[50%]' src={getCookie('profileImage')} alt="프로필 이미지" />
-    <p> {name}님 어서오세요 !</p>
+    <p> {getCookie('nickname')}님 어서오세요 !</p>
+    <button onClick={isLogout}>로그아웃</button>
   </div>
   )
 }
