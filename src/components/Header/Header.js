f1d6eadbfbbda9b478b640b4f503a3caf8a930d6
박지윤 <tkrhkfld123@naver.com>
2023-11-05 14:13:10 +0900

    ➕ [ADD] GPT 바로가기 구현

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ import { useRouter } from 'hooks/useRouter';
 import { headerContent } from 'router';
 import { useNavigate } from 'react-router-dom';
 import { getCookie } from 'components/Cookie/Cookies';
+import ChatGPT from '../../images/gptImg.svg'
 
 export default function Header() {
 
@@ -18,12 +19,24 @@ export default function Header() {
     routeTo(path)
   }
 
+  const [isHovered, setIsHovered] = useState(false);
+
+
   return (
     <div className='flex w-full h-16 shadow-md'>
       <div className='flex items-center justify-start w-[10%] ml-4'>
-        <a href='/' className='p-2 border border-orange-500 rounded-xl'>
+        {/* <a href='/' className='p-2 border border-orange-500 rounded-xl'>
           <AiOutlineMenu size='24' />
-        </a>
+        </a> */}
+        <img className='cursor-pointer' src={ChatGPT} alt='gpt 바로가기 버튼'
+        onClick={() => window.open("https://chat.openai.com/")} 
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}></img>
+        {isHovered && (
+          <div className="absolute p-2 text-slate-800 transform -translate-x-1/2 -translate-y-1/2 bg-green-100 rounded top-[32px] left-[105px] opacity-50">
+            <p className='text-xs'>GPT 바로가기</p>
+          </div>
+        )}
       </div>
       <div className='flex items-center justify-start w-4/5 text-base font-bold gap-x-12 grow'>
         {headerContent.map((element) => {
