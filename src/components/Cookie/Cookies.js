6cf64a53fed41657be43ffef0447fb526f02680d
박지윤 <tkrhkfld123@naver.com>
2023-11-06 16:04:08 +0900

    ✅ [MOD] cookie 수정

--- src/components/Cookie/Cookies.js
+++ src/components/Cookie/Cookies.js
@@ -6,10 +6,10 @@ export const setCookie = (name, value, options) => {
  	return cookies.set(name, value, {...options}); 
 }
 
-export const getCookie = (name) => {
- return cookies.get(name); 
+export const getCookie = (name, value, options) => {
+ return cookies.get(name, value, {...options}); 
 }
 
-export const removeCookie = (name) => {
-	return cookies.remove(name)
+export const removeCookie = (name, value, options) => {
+	return cookies.remove(name, value, {...options})
 }
