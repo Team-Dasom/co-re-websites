1e59a6bbbbb4464278144e8931d1c6c9633fd555
Song Jae Seok <106071687+Largopie@users.noreply.github.com>
2023-11-09 15:32:40 +0900

    [MOD] 기능별 언어 선택 옵션 수정 (#68)
    * ✅ [MOD] 변수명 추천 언어 일 부 수정
    
    - C, Java, JavaScript, Python, Dart, Go, Ruby로 변경. 중복이 예상되는 언어 제외.
    
    * ✅ [MOD] 주석 생성, 코드 리팩토링 기능 언어 옵션 일부 수정
    
    - Python, C, C++, Java, Ruby, Kotlin, Swift, C#, JavaScript, TypeScript, Go, D, Rust, C++, Dart
    
    * ✅ [MOD] 알고리즘 문제 해결 언어 선택 옵션 일부 수정

--- src/pages/AddCommentPage.js
+++ src/pages/AddCommentPage.js
@@ -1,17 +1,17 @@
-import React, { useCallback, useEffect, useRef} from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import Conversation from 'components/conversation/Conversation';
 import { addAnswer, addQuestion } from 'store/addComment/addCommentSlice';
 import TextAreaForm from 'components/Form/TextAreaForm';
 
 export default function AddComment() {
-  const languageList = ['Python3', 'C', 'Java', 'Ruby', 'Kotlin', 'Swift', 'C#', 'JavaScript', 'TypeScript', 'Go', 'D', 'Rust', 'C++'];
+  const languageList = ['Python', 'C', 'C++', 'Java', 'Ruby', 'Kotlin', 'Swift', 'C#', 'JavaScript', 'TypeScript', 'Go', 'D', 'Rust', 'Dart'];
   const conversation = useSelector((state) => state.addComment.conversation);
   const chatBoxRef = useRef();
 
-  const scrollToBottom  = useCallback(() => {
-    chatBoxRef.current.scrollIntoView({behavior: 'smooth', block: 'end', inline: 'nearest'})
-  }, [conversation])
+  const scrollToBottom = useCallback(() => {
+    chatBoxRef.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+  }, [conversation]);
 
   useEffect(() => {
     scrollToBottom();
@@ -23,11 +23,12 @@ export default function AddComment() {
         return <Conversation key={item.id} data={item} isAnswer={item.isAnswer} />;
       })}
       <TextAreaForm
-      camelCaseAPI='addComment'
-      placeholder='주석을 삽입할 코드를 입력해주세요.'
-      addQuestion={addQuestion}
-      addAnswer={addAnswer}
-      dropdownList={languageList}/>
+        camelCaseAPI='addComment'
+        placeholder='주석을 삽입할 코드를 입력해주세요.'
+        addQuestion={addQuestion}
+        addAnswer={addAnswer}
+        dropdownList={languageList}
+      />
       <div className='flex-shrink-0 h-36 md:h-48'></div>
     </div>
   );
--- src/pages/RecommandVariableNamePage.js
+++ src/pages/RecommandVariableNamePage.js
@@ -5,32 +5,25 @@ import Answer from 'components/conversation/Conversation';
 import TextAreaForm from 'components/Form/TextAreaForm';
 
 export default function RecommandVariableName() {
-  const conversation = useSelector(
-    (state) => state.variebleName.conversation,
-  );
-  const languageList = ['C', 'C#', 'C++', 'Dart', 'Go', 'Java', 'Javascript', 'typescript', 'kotlin'];
+  const conversation = useSelector((state) => state.variebleName.conversation);
+  const languageList = ['C', 'Java', 'JavaScript', 'Python', 'Dart', 'Go', 'Ruby'];
   const chatBoxRef = useRef();
 
-  const scrollToBottom  = useCallback(() => {
-    chatBoxRef.current.scrollIntoView({behavior: 'smooth', block: 'end', inline: 'nearest'})
-  }, [conversation])
+  const scrollToBottom = useCallback(() => {
+    chatBoxRef.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+  }, [conversation]);
 
   useEffect(() => {
     scrollToBottom();
   }, [conversation]);
 
   return (
-      <div ref={chatBoxRef}>
-        {conversation.map((item) => {
-          return <Answer key={item.id} data={item} isAnswer={item.isAnswer} />;
-        })}
-        <TextAreaForm
-          camelCaseAPI='recommendVariableName'
-          placeholder='추천을 원하는 "변수"명만 입력해 보세요.'
-          addQuestion={addQuestion}
-          addAnswer={addAnswer}
-          dropdownList={languageList}/>
-        <div className='flex-shrink-0 h-36 md:h-48'></div>
+    <div ref={chatBoxRef}>
+      {conversation.map((item) => {
+        return <Answer key={item.id} data={item} isAnswer={item.isAnswer} />;
+      })}
+      <TextAreaForm camelCaseAPI='recommendVariableName' placeholder='추천을 원하는 "변수"명만 입력해 보세요.' addQuestion={addQuestion} addAnswer={addAnswer} dropdownList={languageList} />
+      <div className='flex-shrink-0 h-36 md:h-48'></div>
     </div>
-  )
+  );
 }
