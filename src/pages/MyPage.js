2e32b83762cdea0ff0413790a5e7541836ed4b16
박지윤 <tkrhkfld123@naver.com>
2023-11-09 19:39:39 +0900

    ✅ [MOD] 마이페이지 사진 크기 조절 및 텍스트 위치 변경

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -16,10 +16,10 @@ export default function MyPage() {
   return (
   <div>
     <div className='grid place-items-center w-[636px] h-auto text-center ml-[389px] mt-[40px] mb-[40px] border-[1px] border-solid border-[#3B82F6] p-[40px] rounded-xl'>
-        <h1 className='font-semibold'>기본 정보</h1>
+        <h1 className='font-semibold ml-[-470px]'>기본 정보</h1>
         <div className='bg-blue-50 w-[560px] p-[30px] rounded-xl'>
-          <img className='ml-[98px] w-[300px] h-[300px] rounded-[50%]' src={getCookie('profileImage')} alt="프로필 이미지" />
-          <p className='font-semibold'>{getCookie('nickname')}</p>
+          <img className='ml-[178px] w-[150px] h-[150px] rounded-[50%]' src={getCookie('profileImage')} alt="프로필 이미지" />
+          <p className='mt-4 font-semibold'>{getCookie('nickname')}</p>
         </div>
         <div className='mt-[10px]'>
           <div className='bg-blue-50 w-[560px] p-[30px] rounded-xl'>
