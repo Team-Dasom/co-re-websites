517fd9d9d7c67e5373625a822767ff189c5c4882
박지윤 <tkrhkfld123@naver.com>
2023-11-09 19:06:02 +0900

    💄 [UI] 기능 컴포넌트 클릭 시 해당 컴포넌트만 렌더링해서 보여주기

--- src/pages/Bookmark.js
+++ src/pages/Bookmark.js
@@ -1,23 +1,72 @@
-import BookmarkList from 'components/Bookmark/BookmarkList';
-import React from 'react';
+import { useState } from 'react';
+import VariableBookmark from 'components/Bookmark/VariableBookmark';
+import AlgorithmsBookmark from 'components/Bookmark/AlgorithmsBookmark';
+import RefactorBookmark from 'components/Bookmark/RefactorBookmark';
+import AddComentBookmark from 'components/Bookmark/AddComentBookmark';
 
-const Bookmark = () => {
-    return (
-    <div>
-      {/* 왼쪽 사이드바 */}
-        <BookmarkList />
 
-      {/* 메인 콘텐츠 */}
-        <div className="w-3/4 p-4">
-            <h1>메인 콘텐츠</h1>
+
+function Bookmark() {
+  const [content, setContent] = useState();
+
+  const handleClickButton = e => {
+    console.log(e.target)
+    const { name } = e.target;
+    setContent(name);
+  };
+
+  const selectComponent = {
+    first: <VariableBookmark />,
+    second: <AlgorithmsBookmark />,
+    third: <RefactorBookmark />,
+    fourth: <AddComentBookmark />,
+  };
+
+  console.log(content);
+
+  return (
+    <div className='flex'>
+        <div className='h-screen p-4 text-white bg-gray-800 w-[260px] flex flex-col'>
+        <h1 className='mb-[60px] text-2xl text-center'>요청내용 즐겨찾기</h1>
+            {MAIN_DATA.map(data => {
+            return (
+                    <button className={`mb-5 text-left ${content === data.name ? 'text-blue-500 font-bold' : ''}`}
+                        onClick={handleClickButton} name={data.name} key={data.id}>
+                        {data.text}
+                    </button>
+                );
+            })}
+    </div>
+        <div>
+        {content && <div>{selectComponent[content]}</div>}
         </div>
     </div>
-    );
-}
+  );
+};
+
 
 export default Bookmark;
 
 
-
-
-
+export const MAIN_DATA = [
+  {
+    id: 1,
+    text: '변수명 추천',
+    name: 'first',
+  },
+  {
+    id: 2,
+    text: '알고리즘 문제 해설',
+    name: 'second',
+  },
+  {
+    id: 3,
+    text: 'Code 리팩토링',
+    name: 'third',
+  },
+  {
+    id: 4,
+    text: 'Code 주석 생성',
+    name: 'fourth',
+  },
+];
