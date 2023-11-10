c33c4d673a9110748a08b5cd6b61a7f505ef8eec
박지윤 <tkrhkfld123@naver.com>
2023-11-10 19:38:53 +0900

    ✨ [FEAT] 로그인, 기능별 페이지 유지

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import { configureStore} from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 import userSlice from 'store/KakaoLogin/kakaoUserSlice';
 import tokenReducer from 'store/KakaoLogin/tokenSlice';
 import statusReducer from './KakaoLogin/statusSlice';
@@ -7,16 +10,27 @@ import variebleNameSlice from 'store/variebleName/variebleNameSlice';
 import addCommentSlice from './addComment/addCommentSlice';
 import refactorCodeSlice from './refactorCode/refactorCodeSlice';
 
-const store =  configureStore({
-  reducer: {
-    user: userSlice,
-    token: tokenReducer,
-    status: statusReducer,
-    changeLanguage: changeLanguageSlice,
-    variebleName: variebleNameSlice,
-    addComment: addCommentSlice,
-    refactorCode: refactorCodeSlice
-  },
+const reducers = combineReducers({
+  user: userSlice,
+  token: tokenReducer,
+  status: statusReducer,
+  changeLanguage: changeLanguageSlice,
+  variebleName: variebleNameSlice,
+  addComment: addCommentSlice,
+  refactorCode: refactorCodeSlice
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user','status','token','variebleName','changeLanguage','addComment','refactorCode']
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
 
 export default store;
