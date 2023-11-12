8e873212ace2a7d559c4646e72799571f1e0e626
박지윤 <tkrhkfld123@naver.com>
2023-11-12 19:07:55 +0900

    ⚰️ [DEL] 코드 삭제

--- src/App.js
+++ src/App.js
@@ -23,20 +23,6 @@ export default function App() {
   }
   hideConsoleMessage("Element previously highlighted. To highlight again, first unset `dataset.highlighted`","WARN:");
   hideConsoleMessage("WARN:");
-
-  // 에러 메시지가 특정 패턴과 일치하는지 확인하는 함수
-function isNonSerializableError(error) {
-  return error.message.startsWith("A non-serializable value was detected in an action");
-}
-
-// 에러 이벤트를 감지하여 특정 조건일 때 콘솔에 출력하지 않음
-window.addEventListener('error', function (event) {
-  if (isNonSerializableError(event.error)) {
-    // 특정 에러 메시지일 경우 아무 작업도 수행하지 않음
-    // 즉, 콘솔에 에러를 표시하지 않음
-    event.preventDefault();
-  }
-});
   return (
     <>
       <RouterProvider router={routers} />
